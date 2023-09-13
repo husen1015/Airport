c2c8042fec827c76f67e78afc17842485a8edefe
husen1015 <husen.ab1015@gmail.com>
2023-09-13 15:47:59 +0200

    bugfix, currently only one checkIn counter works

--- Airport/Assets/GOAP scripts/Actions/CheckIn.cs
+++ Airport/Assets/GOAP scripts/Actions/CheckIn.cs
@@ -6,11 +6,16 @@ public class CheckIn : Action
 {
     public override bool PostPrefom()
     {
+        //add 1 to available counters
+        var ws = GameWorld.Instance.GetWorldStates1().GetStates();
+        int availableCounters = ws["AvailableCounters"];
+        GameWorld.Instance.GetWorldStates1().SetState("AvailableCounters", availableCounters + 1);
         return true;
     }
 
     public override bool PrePrefom()
     {
+        //remove one from available counters
         Debug.Log($"checking in, curr available counters: {GameWorld.Instance.GetWorldStates1().GetStates()["AvailableCounters"]}");
         var ws = GameWorld.Instance.GetWorldStates1().GetStates();
         int availableCounters = ws["AvailableCounters"];
