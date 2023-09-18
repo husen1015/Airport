53b3b6e5b05d8899492c4d68ee8dff7ef958ffe0
husen1015 <husen.ab1015@gmail.com>
2023-09-19 00:10:22 +0200

    added some functionality for simulating queues in security check

--- Airport/Assets/GOAP scripts/Actions/SecurityCheck.cs
+++ Airport/Assets/GOAP scripts/Actions/SecurityCheck.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class SecurityCheck : Action
 {
+    public SecurityCounter sc;
+    public int placeInLine;
+    bool postPreformed = false;
+
     public override bool PostPrefom()
     {
+        postPreformed= true;
+        sc.AdvanceQueue();
+        Debug.Log("post security");
         return true;
     }
 
     public override bool PrePrefom()
     {
+        var lineInfo = sc.admitNewPassenger(this);
+        target = lineInfo.Item1.gameObject;
+        placeInLine = lineInfo.Item2;
+        duration = lineInfo.Item3;
         return true;
     }
 
@@ -23,6 +35,9 @@ public class SecurityCheck : Action
     // Update is called once per frame
     void Update()
     {
-        
+        if (!postPreformed)
+        {
+            agent.SetDestination(target.transform.position);
+        }
     }
 }
--- Airport/Assets/Scripts/Passenger.cs
+++ Airport/Assets/Scripts/Passenger.cs
@@ -11,7 +11,7 @@ public class Passenger : GameAgent
         base.Start();
         tiredness = 0;
         //each passenger has 50% chance to be tired in which case it will priorotize resting above checking in
-        int tired = Passengers.getRandomInt(0, 2);
+        //int tired = Passengers.getRandomInt(0, 2);
 
 
         //SubGoal goal1 = new SubGoal("rested", 4, false);
@@ -19,30 +19,30 @@ public class Passenger : GameAgent
 
         SubGoal goal1;
         SubGoal goal2;
-        Debug.Log(tired);
-        if (tired == 1)
-        {
-            tiredness = Passengers.getRandomInt(3, 7);
-            goal1 = new SubGoal("rested", 4, false);
-            goal2 = new SubGoal("CheckedIn", 3, false);
-            SubGoals.Add(goal1, 4);
-            SubGoals.Add(goal2, 3);
-        }
-        else
-        {
-            goal1 = new SubGoal("rested", 3, false);
-            goal2 = new SubGoal("CheckedIn", 4, false);
-            SubGoals.Add(goal1, 3);
-            SubGoals.Add(goal2, 4);
-        }
+        //Debug.Log(tired);
+        //if (tired == 1)
+        //{
+        //    tiredness = Passengers.getRandomInt(3, 7);
+        //    goal1 = new SubGoal("rested", 4, false);
+        //    goal2 = new SubGoal("CheckedIn", 3, false);
+        //    SubGoals.Add(goal1, 4);
+        //    SubGoals.Add(goal2, 3);
+        //}
+        //else
+        //{
+        //    goal1 = new SubGoal("rested", 3, false);
+        //    goal2 = new SubGoal("CheckedIn", 4, false);
+        //    SubGoals.Add(goal1, 3);
+        //    SubGoals.Add(goal2, 4);
+        //}
 
         SubGoal goal3 = new SubGoal("PassedSecurity", 6, false);
         SubGoal goal4 = new SubGoal("ReadyToBoard", 8, false);
 
 
 
-        //SubGoals.Add(goal3, 6);
-        //SubGoals.Add(goal4, 8);
+        SubGoals.Add(goal3, 6);
+        SubGoals.Add(goal4, 8);
 
         //SubGoal goal1 = new SubGoal("checkin", 3, false);
         //SubGoal goal2 = new SubGoal("passSecurity", 3, false);
